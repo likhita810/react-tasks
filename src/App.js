d0ad285cb13367bac09aaacfd4c3dce3561b5de7
likhita810 <klikhita@lyrostech.com>
2024-08-16 15:12:12 +0530

    aug-16-added router to portfolio task

--- src/App.js
+++ src/App.js
@@ -9,12 +9,22 @@ import MainComp from "./task11-cnct-mngr-login/Components/MainComp";
 import CountComp from './task12-count-comp/CountComp';
 import TimeComp from './task12-count-comp/time';
 import TodoMain from './task13-to-do-app-hoc/Main';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       {/* <Nav fName="Likhita" lName="Konathala"></Nav>
       <Profile></Profile> */}
+
+      <BrowserRouter>
+        <Link to='/navbar'>Nav</Link>
+        <Link to='/profile'>Profile</Link>
+        <Routes>
+          <Route path='/navbar' element={<Nav></Nav>}></Route>
+          <Route path='/profile' element={<Profile></Profile>}></Route>
+        </Routes>
+      </BrowserRouter>
 
       {/* <CounterApplication></CounterApplication> */}
 
@@ -23,7 +33,7 @@ function App() {
 
       {/* <CountComp></CountComp> */}
       {/* <TimeComp msg='this is a prop value updated from parent comp' ></TimeComp> */}
-      <TodoMain></TodoMain>
+      {/* <TodoMain></TodoMain> */}
 
     </div>
   );
